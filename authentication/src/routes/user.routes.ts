f166f13d6007d6b886a0a25b0a59f2bf0bc9ba44
RomitGandhi07 <gandhiromit77@gmail.com>
2024-09-11 23:15:36 +0530

    Setted up request body middleware and added signup request validation

--- authentication/src/routes/user.routes.ts
+++ authentication/src/routes/user.routes.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { registerUser } from "../controllers/users/register.controller";
+import { UserRegisterValidation } from "../validations/users/register.validation";
+import { validateRequest } from "../middlewares/validate-request.middleware";
 const router = express.Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    UserRegisterValidation(),
+    validateRequest,
+    registerUser
+);
+
 // router.route("/login").post(userLoginValidator(), validate, loginUser);
 // router.route("/refresh-token").post(refreshAccessToken);
 
